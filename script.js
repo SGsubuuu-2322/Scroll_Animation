a8c28c32f46d8ba877a3e27f48db50f4d50588f3
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-10-30 23:35:49 +0530

    Added the meaningful comments for the script file in this project...

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// This is the constant containing the box HTML for further modifications...
 const boxes = document.querySelectorAll(".box");
+
+// Adding the scroll eventlisteners to the windows...
 window.addEventListener("scroll", checkBoxes);
 
 checkBoxes();
 
+// This checkBoxes() function is written for dynamically adding scroll animation to the windows...
 function checkBoxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
 
